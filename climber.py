67ae51a3b61fa00502c45419f9e3a2a5f6e58ddc
Swifty-Dog <70727050+Swifty-Dog@users.noreply.github.com>
2023-01-12 01:09:24 +0100

    Update climber.py

--- climber.py
+++ climber.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-
+from datetime import datetime,date
 class Climber:
     def __init__(self, ID, first_name, last_name, nationality, date_of_birth) -> None:
         self.id = ID
@@ -9,11 +9,40 @@ class Climber:
         self.nationality = nationality
         self.date_of_birth = date_of_birth
 
-    def test(self):
-        return f"{self.first_name} {self.last_name}"
+    def get_age(self):
+        today = date.today()
+        self.date_of_birth = self.date_of_birth.split("-")
+        age= today.year - int(self.date_of_birth[2]) - ((today.month, today.day) < (int(self.date_of_birth[1]), int(self.date_of_birth[0])))
+
+        return age
+
+    def get_expeditions(self):
+        expedition_list = []
+        conn = sqlite3.connect("climbersapp.db")
+        c = conn.cursor()
+        c.execute("SELECT name FROM expeditions WHERE success = true AND id = ?", (self.id,))
+        expeditions = c.fetchall()
+        for expedition in expeditions:
+            expedition_list.append(expedition)
+        conn.commit()
+        return expedition_list
+
+        # conn = sqlite3.connect("climbersapp.db")
+        # cursor = conn.cursor()
+        # query = "SELECT * FROM expeditions WHERE climber_id = ? AND success = 1 "
+        # val = self.id
+        # cursor.execute(query, val)
+        # expeditions = cursor.fetchall()
+        # conn.close()
+        # return expeditions
+
+
+climb1 = Climber(931,"Burr", "Danahar","Malawi","06-12-1907")
+
 
 def main():
-    pass
+    print(climb1.get_expeditions())
+
 
 # Representation method
 # This will format the output in the correct order
@@ -23,3 +52,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
